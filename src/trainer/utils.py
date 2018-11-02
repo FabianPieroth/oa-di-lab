69b62a1539585af9df35943c3213dd1066c6286f
ga53dur <eva.m.hoeck@tum.de>
2018-11-02 08:59:56 +0100

    modify scale and center utisl for batch

--- src/trainer/utils.py
+++ src/trainer/utils.py
@@ -1,13 +1,32 @@
-def scale_image(image, min_data, max_data, min_out=-1, max_out=1):
-    """ Scales the image from range [min_data, max_data] to [min_out,max_out]
-        Now only for 2dim data (US)"""
-    factor = (max_out-min_out)/(max_data - min_data)
-    additive = min_out - min_data*(max_out-min_out)/(max_data - min_data)
-    image_out = image*factor + additive
+def scale_image(image, min_data, max_data, image_type, min_out=-1, max_out=1):
+    if image_type == 'US':
+        factor = (max_out-min_out)/(max_data - min_data)
+        additive = min_out - min_data*(max_out-min_out)/(max_data - min_data)
+        image_out = image*factor + additive
+    else:
+        print('scale and center not implemented yet for OA data')
     return image_out
 
-def scale_image_reverse(scaledimage, min_data, max_data, min_in= -1, max_in = 1):
-    """ Reverses the scaling, transforms the image back to [min_data, max_data]
-        Now only for 2dim data (US)"""
-    image_out = scale_image(scaledimage, min_in, max_in, min_data, max_data)
-    return image_out
+def scale_batch(batch, min_data, max_data, image_type, min_out=-1, max_out=1):
+    if image_type == 'US':
+        factor = (max_out-min_out)/(max_data - min_data)
+        additive = min_out - min_data*(max_out-min_out)/(max_data - min_data)
+        batch_out = batch*factor + additive
+    else: print('scale and center not implemented yet for OA data')
+    return batch_out
+
+def scale_and_center(batch, scale_params, mean_image, image_type):
+    if image_type == 'US':
+        [min_data, max_data] = scale_params
+        mean_scaled = scale_image(mean_image, min_data, max_data, image_type = image_type)
+        batch_out = scale_batch(batch, min_data, max_data)
+        batch_out = image_out - mean_scaled
+    else: print('scale and center not implemented yet for OA data')
+    return batch_out
+
+def scale_and_center_reverse(batch, scale_params, mean_image):
+    [min_data, max_data] = scale_params
+    mean_scaled = scale_image(mean_image, min_data, max_data)
+    batch_out = batch + mean_scaled
+    batch_out = scale_batch(batch_out, -1,1,min_data, max_data)
+    return batch_out
