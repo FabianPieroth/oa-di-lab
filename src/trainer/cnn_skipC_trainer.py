5a962a98fe39e7524cf4b2463384b599fecc5028
ga53dur <eva.m.hoeck@tum.de>
2018-11-03 11:14:52 +0100

    tested get_scale_and_center for OA data, fixed param import for OA

--- src/trainer/cnn_skipC_trainer.py
+++ src/trainer/cnn_skipC_trainer.py
@@ -1,8 +1,10 @@
 from data.data_loader import ProcessData
 import trainer.utils as utils
+from matplotlib import pyplot as plt
+#import numpy as np
 
 class CNN_skipCo_trainer(object):
-    def __init__(self, image_type='US'):
+    def __init__(self, image_type='OA'):
         self.image_type = image_type
         self.dataset = ProcessData(train_ratio=0.7,process_raw_data=False, image_type=self.image_type, get_scale_center=False)
 
@@ -12,20 +14,22 @@ class CNN_skipCo_trainer(object):
     def fit(self):
         self.dataset.batch_names(batch_size=5) # call this to separate names into random batches
         # get scale and center parameters
-        scale_params_low, scale_params_high = utils.load_params(image_type=self.image_type, param_type="scale")
-        mean_image_low, mean_image_high = utils.load_params(image_type=self.image_type, param_type="mean_image")
+        scale_params_low, scale_params_high = utils.load_params(image_type=self.image_type, param_type="scale_params")
+        mean_image_low, mean_image_high = utils.load_params(image_type=self.image_type, param_type="mean_images")
+
         # in self.batch_number is the number of batches in the training set
         for i in range(self.dataset.batch_number):
             X, Y = self.dataset.create_train_batches(self.dataset.train_batch_chunks[i])
             print("The input batch:")
             print(X.shape)
             # scale and center the batch
-            scale_center_X = utils.scale_and_center(X, scale_params_low, mean_image_low, image_type = self.image_type)
-            scale_center_Y = utils.scale_and_center(Y, scale_params_high, mean_image_high, image_type=self.image_type)
+        #    scale_center_X = utils.scale_and_center(X, scale_params_low, mean_image_low, image_type = self.image_type)
+        #    scale_center_Y = utils.scale_and_center(Y, scale_params_high, mean_image_high, image_type=self.image_type)
 
             ## how to undo the scaling:
             #unscaled_X = utils.scale_and_center_reverse(scale_center_X, scale_params_low, mean_image_low, image_type = self.image_type)
             #unscaled_Y = utils.scale_and_center_reverse(scale_center_Y, scale_params_high, mean_image_high, image_type=self.image_type)
+            #print(np.max(np.abs(unscaled_X - X))) # you have to import numpy first
 
 
 
