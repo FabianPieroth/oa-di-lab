a22e2e74f62c6eb5fd92224c53890b197a336c33
Tom Wollschläger <ga35teh@mytum.de>
2018-11-23 12:11:49 +0100

    adds model superclass for a flexible way of initializing models with flexible nr of layers and channels, strides, padding, kernel_sizes. Also adds a trainer class which will replace all trainers from now

---
+++ src/models/model_superclass.py
@@ -0,0 +1,126 @@
+import sys
+import torch
+import torch.nn as nn
+from torch.nn.functional import relu
+
+class ConvLayer(nn.Module):
+    def __init__(self, c_in, c_out, stride=2, kernel_size=(5,5), padding=2):
+        super().__init__()
+        self.conv = nn.Conv2d(c_in,c_out,stride=stride, kernel_size=kernel_size, padding=padding).double()
+        self.bn = nn.BatchNorm2d(c_out).double()
+
+
+    def forward(self, x):
+        return relu(self.bn(self.conv(x)))
+
+class DeConvLayer(nn.Module):
+    """ No RELU BEHIND DECONV DUE TO THE SKIP CONNECTIONS"""
+    def __init__(self, c_in, c_out, stride=2, kernel_size=(5,5), padding=2):
+        super().__init__()
+        self.deconv = nn.ConvTranspose2d(c_in, c_out, stride=stride, kernel_size=kernel_size, padding=padding).double()
+        self.bn = nn.BatchNorm2d(c_out).double()
+
+
+    def forward(self, x):
+        return self.bn(self.deconv(x))
+
+
+
+class ImageTranslator(nn.Module):
+
+    def __init__(self, conv_channels, strides=None, kernels=None, padding=None,
+                 criterion=nn.MSELoss(), optimizer=torch.optim.Adam, learning_rate=0.01, model_name='shallow_model'):
+        """
+        initializes net with the specified attributes. The stride, kernels and paddings and output paddings for the
+        deconv layers are computed to fit.
+        :param conv_channels: output channels of the convolution layers
+        :param deconv_channels:
+        :param strides: stride sizes of the conv layers, same dimension as conv_channels
+        :param kernels: kernel sizes of the conv layers, same dimension as conv_channels
+        """
+        super().__init__()
+
+
+        self.model_file_name = __file__  # save file name to copy file in logger into logging folder
+
+        if strides is None:
+            # initialize list with default strides (2,2)
+            default_stride = 2
+            strides = [default_stride for i in range(len(conv_channels))]
+        if kernels is None:
+            # initialize list with default kernels (5,5)
+            default_kernel = (5,5)
+            kernels = [default_kernel for i in range(len(conv_channels))]
+        if padding is None:
+            padding = [2 for i in range(len(conv_channels))]
+
+        deconv_strides, deconv_kernels, padding, opad = self.compute_strides_and_kernels(strides, kernels, padding)
+
+
+        self.conv_layers = nn.ModuleList([ConvLayer(conv_channels[i], conv_channels[i + 1],
+                                                    strides[i], kernels[i])
+                                          for i in range(len(conv_channels) - 1)])
+
+        deconv_channels = conv_channels[::-1]
+        self.deconv_layers = nn.ModuleList([DeConvLayer(deconv_channels[i], deconv_channels[i + 1],
+                                                        deconv_strides[i], deconv_kernels[i])
+                                            for i in range(len(deconv_channels) - 1)])
+
+        self.criterion = criterion
+        self.optimizer = optimizer(self.parameters(), lr=learning_rate)
+        self.train_loss = []
+        self.val_loss = []
+        self.model_name = model_name
+
+    def forward(self, x):
+
+        skip_connection = []
+
+        for i in range(len(self.conv_layers)):
+            if i%2==0:
+                skip_connection += [x]
+            else:
+                skip_connection += [0]
+
+            l = self.conv_layers[i]
+            x = l(x)
+        # adding a 0 for the last 'connection' between conv and deconv
+        skip_connection += [0]
+
+        for i in range(len(self.deconv_layers)):
+            l = self.deconv_layers[i]
+            skip = skip_connection[len(skip_connection)-1-i]
+            x = relu(l(x+skip))
+
+        return x
+
+
+    def train_model(self, X, y, current_epoch=None):
+
+        def closure():
+            self.optimizer.zero_grad()
+            out = self.forward(X)
+            loss = self.criterion(out, y)
+            if current_epoch is not None:
+                sys.stdout.write('\r' + ' epoch ' + str(current_epoch) + ' |  loss : ' + str(loss.item()))
+            else:
+                sys.stdout.write('\r  loss : ' + str(loss.item()))
+            self.train_loss.append(loss.item())
+            loss.backward()
+            return loss
+
+        self.optimizer.step(closure)
+        return
+
+
+    def set_learning_rate(self, learning_rate):
+        """
+        setting the learning rate. Can be explicitly done to have another learning rate without new initialization
+        :param learning_rate: learning rate to be set
+        """
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = learning_rate
+
+
+    def compute_strides_and_kernels(self, strides, kernels, padding):
+        return strides, kernels, padding, [0 for i in range(len(strides))]
---
+++ src/trainer/trainer.py
@@ -0,0 +1,220 @@
+from data.data_loader import ProcessData
+from logger.logger import Logger
+import numpy as np
+from models.model_superclass import ImageTranslator
+import torch
+import torch.nn as nn
+import sys
+import matplotlib.pyplot as plt
+
+class CNN_skipCo_trainer(object):
+    def __init__(self):
+
+        self.image_type = 'US'
+
+        self.dataset = ProcessData(train_ratio=0.9, process_raw_data=False,
+                                   do_augment=False, add_augment=False,
+                                   do_flip=True, do_blur=True, do_deform=True, do_crop=True,
+                                   image_type=self.image_type, get_scale_center=False, single_sample=False)
+
+        self.model = ImageTranslator(conv_channels=[1, 32, 64, 128, 256])
+        if torch.cuda.is_available():
+            torch.cuda.current_device()
+            self.model.cuda()
+
+        self.logger = Logger(model=self.model, project_root_dir=self.dataset.project_root_dir,
+                             image_type=self.image_type)
+        self.epochs = 250
+
+    def fit(self, learning_rate, use_one_cycle=False):
+        # get scale and center parameters
+        scale_params_low, scale_params_high = self.dataset.load_params(param_type="scale_params")
+        mean_image_low, mean_image_high = self.dataset.load_params(param_type="mean_images")
+
+        # currently for one image:
+        '''
+        self.dataset.batch_names(batch_size=5)
+        X, Y = self.dataset.create_train_batches(self.dataset.train_batch_chunks[1])
+        print(X.shape)
+        X = X[0,:,:]
+        Y = Y[0,:,:]
+        '''
+        # load validation set, normalize and parse into tensor
+
+        input_tensor_val, target_tensor_val = self.dataset.scale_and_parse_to_tensor(
+            batch_files=self.dataset.val_file_names,
+            scale_params_low=scale_params_low,
+            scale_params_high=scale_params_high,
+            mean_image_low=mean_image_low,
+            mean_image_high=mean_image_high)
+
+        if torch.cuda.is_available():
+            input_tensor_val = input_tensor_val.cuda()
+            target_tensor_val = target_tensor_val.cuda()
+
+        if use_one_cycle:
+            higher_rate = learning_rate
+            lower_rate = 1 / 10 * higher_rate
+
+            num_epochs = self.epochs
+            up_num = int((num_epochs -50 ) /2)
+            down_num = up_num
+            ann_num = num_epochs - up_num - down_num
+            lr_up = np.linspace(lower_rate, higher_rate, num=up_num)
+            lr_down = np.linspace(higher_rate, lower_rate, num=down_num)
+            lr_anihilating = np.linspace(lower_rate, 0, num=ann_num)
+            learning_rates = np.append(np.append(lr_up, lr_down), lr_anihilating)
+
+        else:
+            self.model.set_learning_rate(learning_rate)
+
+
+        for e in range(0, self.epochs):
+            if use_one_cycle:
+                lr = learning_rates[e]
+                self.model.set_learning_rate(lr)
+            # separate names into random batches and shuffle every epoch
+            self.dataset.batch_names(batch_size=32)
+
+            # in self.batch_number is the number of batches in the training set
+            for i in range(self.dataset.batch_number):
+                input_tensor, target_tensor = self.dataset.scale_and_parse_to_tensor(
+                    batch_files=self.dataset.train_batch_chunks[i],
+                    scale_params_low=scale_params_low,
+                    scale_params_high=scale_params_high,
+                    mean_image_low=mean_image_low,
+                    mean_image_high=mean_image_high)
+
+                if torch.cuda.is_available():
+
+                    input_tensor = input_tensor.cuda()
+                    target_tensor = target_tensor.cuda()
+
+                self.model.train_model(input_tensor, target_tensor, current_epoch=e)
+
+            # calculate the validation loss and add to validation history
+            self.logger.get_val_loss(val_in=input_tensor_val, val_target=target_tensor_val)
+            # save model every x epochs
+            if e % 25 == 0 or e == self.epochs - 1:
+                self.logger.log(save_appendix='_epoch_' + str(e),
+                                current_epoch=e,
+                                epochs=self.epochs,
+                                mean_images=[mean_image_low, mean_image_high],
+                                scale_params=[scale_params_low, scale_params_high])
+
+                # how to undo the scaling:
+                # unscaled_X = utils.scale_and_center_reverse(scale_center_X,
+                #  scale_params_low, mean_image_low, image_type = self.dataset.image_type)
+                # unscaled_Y = utils.scale_and_center_reverse(scale_center_Y, scale_params_high,
+                #  mean_image_high, image_type=self.dataset.image_type)
+    def predict(self):
+        # self.model.predict()
+
+        # see self.dataset.X_val and self.dataset.Y_val
+        pass
+
+
+    def find_lr(self, init_value=1e-8, final_value=10., beta=0.98):
+        """
+        learning rate finder. goes through multiple learning rates and does one forward pass with each and tracks the loss.
+        it returns the learning rates and losses so one can make an image of the loss from which one can find a suitable learning rate. Pick the highest one on which the loss is still decreasing (so not the minimum)
+        :param train_in: training data input
+        :param target: training data target
+        :param init_value: inital value which the learning rate start with. init_value < final_value. Default: 1e-8
+        :param final_value: last value of the learning rate. Default: 10.
+        :param beta: used for smoothing the loss. Default: 0.98
+        :return: log_learning_rates, losses
+        """
+
+        # get scale and center parameters
+        scale_params_low, scale_params_high = self.dataset.load_params(param_type="scale_params")
+        mean_image_low, mean_image_high = self.dataset.load_params(param_type="mean_images")
+
+        from torch.autograd import variable as Variable
+        import math
+        self.dataset.batch_names(batch_size=2)
+        print('number of files: ', len(self.dataset.train_file_names))
+        num = self.dataset.batch_number - 1
+        mult = (final_value / init_value) ** (1 / num)
+        lr = init_value
+        self.model.optimizer.param_groups[0]['lr'] = lr
+        avg_loss = 0.
+        best_loss = 0.
+        batch_num = 0
+        losses = []
+        log_lrs = []
+        # for i in range(1, train_in.shape[0]):
+        # in self.batch_number is the number of batches in the training set
+        print('batch numbers: ', self.dataset.batch_number)
+        for i in range(self.dataset.batch_number):
+            sys.stdout.write('\r  current iteration : ' + str(i))
+
+            input_tensor, target_tensor = self.dataset.scale_and_parse_to_tensor(
+                batch_files=self.dataset.val_file_names,
+                scale_params_low=scale_params_low,
+                scale_params_high=scale_params_high,
+                mean_image_low=mean_image_low,
+                mean_image_high=mean_image_high)
+
+            if torch.cuda.is_available():
+                input_tensor = input_tensor.cuda()
+                target_tensor = target_tensor.cuda()
+
+            batch_num += 1
+            # As before, get the loss for this mini-batch of inputs/outputs
+            self.model.optimizer.zero_grad()
+            outputs = self.model.forward(input_tensor)
+            loss = self.model.criterion(outputs, target_tensor)
+            # Compute the smoothed loss
+            avg_loss = beta * avg_loss + (1 - beta) * loss.item()
+            smoothed_loss = avg_loss / (1 - beta ** batch_num)
+            # Stop if the loss is exploding
+            if batch_num > 1 and smoothed_loss > 4 * best_loss:
+                print('loss exploding')
+                return log_lrs, losses
+            # Record the best loss
+            if smoothed_loss < best_loss or batch_num == 1:
+                best_loss = smoothed_loss
+            # Store the values
+            losses.append(smoothed_loss)
+            log_lrs.append(math.log10(lr))
+            # Do the SGD step
+            loss.backward()
+            self.model.optimizer.step()
+            # Update the lr for the next step
+            lr *= mult
+            self.model.optimizer.param_groups[0]['lr'] = lr
+
+        # Plot the results
+
+
+        lrs = 10 ** np.array(log_lrs)
+        fig, ax = plt.subplots(1)
+        ax.plot(lrs, losses)
+        ax.set_xscale('log')
+        ax.set_xlim((1e-8, 1))
+        ax.figure.show()
+        ax.figure.savefig('learning_rate_finder.png')
+
+        return log_lrs, losses
+
+def main():
+    trainer = CNN_skipCo_trainer()
+    trainer.find_lr()
+    # fit the first model
+    print('---------------------------')
+    print('fitting deep model')
+    trainer.fit(learning_rate=0.001, use_one_cycle=True)
+    trainer.predict()
+    # torch.save(trainer.model, "../../reports/model.pt")
+    # trainer.log_model(model_name=trainer.model.model_name)
+    # print('\n---------------------------')
+
+    # print('finding learning rate')
+    # trainer.find_lr()
+
+
+    print('\nfinished')
+
+if __name__ == "__main__":
+    main()
